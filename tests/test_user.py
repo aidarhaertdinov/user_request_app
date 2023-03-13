589596ffc1decfe63a36339228438d35486ad782
aidarhaertdinov <aidarhaertdinov02@gmail.com>
2023-03-14 01:46:43 +0500

    update test_user.py files

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,37 +3,48 @@ from service import user_login
 
 
 def test_create_user():
-    user = {'email': 'user5@mail.ru',
-            'password': 'user5',
+    user = {'email': 'pytest_user@mail.ru',
+            'password': 'pytest123',
             'permission': 'USER',
-            'username': 'Кtytr'
+            'username': 'Pytest_user'
             }
-    response = requests.post('http://127.0.0.1:5000/rest/v1/create_user',
-                             json=user,
-                             headers={'Authorization': user_login()})
+    response_create_user = requests.post('http://127.0.0.1:5000/rest/v1/create_user',
+                                         json=user,
+                                         headers={'Authorization': user_login()})
 
-    assert 201 == response.status_code
+    assert 201 == response_create_user.status_code
+
+    response_delete = requests.delete(f"http://127.0.0.1:5000/rest/v1/users/{response_create_user.json().get('id')}",
+                                      headers={'Authorization': user_login()})
+
+    assert 200 == response_delete.status_code
 
 
 def test_update_user():
 
-    user = {'email': 'user10@mail.ru',
-            'password': 'user10',
+    user = {'email': 'pytest_user@mail.ru',
+            'password': 'pytest123',
             'permission': 'USER',
-            'username': 'В'
+            'username': 'Py'
             }
-    response = requests.post('http://127.0.0.1:5000/rest/v1/create_user',
+    response_create_user = requests.post('http://127.0.0.1:5000/rest/v1/create_user',
                              json=user,
                              headers={'Authorization': user_login()})
-    assert 201 == response.status_code
 
-    update_user = {'email': 'user10@mail.ru',
-                   'password': 'user10',
+    assert 201 == response_create_user.status_code
+
+    update_user = {'email': 'pytest_user@mail.ru',
+                   'password': 'pytest123',
                    'permission': 'USER',
-                   'username': 'Виталий'
+                   'username': 'Pytest_user'
                    }
-    response_update = requests.put('http://127.0.0.1:5000/rest/v1/users/14',
+    response_update = requests.put(f"http://127.0.0.1:5000/rest/v1/users/{response_create_user.json().get('id')}",
                                    json=update_user,
                                    headers={'Authorization': user_login()})
 
     assert 200 == response_update.status_code
+
+    response_delete = requests.delete(f"http://127.0.0.1:5000/rest/v1/users/{response_create_user.json().get('id')}",
+                                      headers={'Authorization': user_login()})
+
+    assert 200 == response_delete.status_code
